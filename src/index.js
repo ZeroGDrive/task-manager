d735c7410fb1fa12018de72deb79e5893012e066
ZeroGDrive <ayoubyf@gmail.com>
2020-06-09 14:39:59 +0200

    adding async await

--- src/index.js
+++ src/index.js
@@ -1,65 +1,73 @@
-const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Tasks = require('./models/tasks');
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Tasks = require("./models/tasks");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => res.status(500).send());
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get('/users/:id', (req, res) => {
-  const _id = req.params.id;
-
-  User.findOne(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((err) => res.status(500).send(err));
+app.get("/users/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks', (req, res) => {
-  Tasks.find({})
-    .then((tasks) => res.send(tasks))
-    .catch((err) => res.status(500).send(err));
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Tasks.find({});
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
-  const _id = req.params.id;
-
-  Tasks.findById(_id)
-    .then((task) => {
-      if (!task) {
-        res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((err) => res.status(500).send(err));
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const task = await Tasks.findById(_id);
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
-app.post('/users', (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((error) => res.status(400).send(error));
+app.post("/users", async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.post('/tasks', (req, res) => {
-  const task = new Tasks(req.body);
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch((err) => res.status(400).send(err));
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = await new Tasks(req.body).save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send();
+  }
 });
 
 app.listen(port);
