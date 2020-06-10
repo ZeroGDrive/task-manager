e7bf7497cfdbc02a7258f0c26474c3983991c0ff
Ayoub Alforjani <ayoubyf@gmail.com>
2020-06-10 22:50:47 +0200

    some changes

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -40,6 +41,20 @@ const userSchema = mongoose.Schema({
     },
   },
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login!');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login!');
+  }
+  return user;
+};
 
 // using Middlewares in mongoose and pre means before saving
 userSchema.pre('save', async function (next) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -33,11 +33,23 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password,
+    );
+    res.send(user);
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
 router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
+    allowedUpdates.includes(update),
   );
 
   if (!isValidOperation) {
